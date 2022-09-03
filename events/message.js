8e97fdb62274c70f2cf754e8c884a2d2d58042fa
swaggy-p-jp <ajattmohamedo@gmail.com>
2022-09-02 19:05:24 -0500

    dealing with retweets

--- events/message.js
+++ events/message.js
@@ -1,6 +1,16 @@
 module.exports = {
 	name: 'messageCreate',
 	async execute(msg) {
+        if(msg.content.includes('neort')) {
+            const endIndex = msg.content.indexOf('neort')
+            const retweetUrl = msg.content.substring(0, endIndex)
+
+            const url = new URL(retweetUrl);
+            url.hostname = "fxtwitter.com";
+
+            msg.delete({ timeout: "1000" });
+            msg.channel.send(`${url.href}`);
+        }
         if (msg.content.startsWith("https://twitter.com/")) {
             const url = new URL(msg);
             url.hostname = "fxtwitter.com";
