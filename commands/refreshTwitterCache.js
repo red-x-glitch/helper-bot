df48d88db7cbc2e96ca61c61ade876b0a0214002
swaggy-p-jp <ajattmohamedo@gmail.com>
2022-09-11 21:26:35 -0500

    added new command that gets twitter likes

---
+++ commands/refreshTwitterCache.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { authorId } = require('./../config.json');
+const { writeToFile } = require('../utils/files');
+const { getTweetUrlsFromLikes } = require('../utils/twitter');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('refresh_twitter_cache')
+		.setDescription('Refreshes Twitter Cache'),
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
+		const returnReply = async (err = undefined) => err ? await interaction.editReply('There was an error') : await interaction.editReply('Cache Refreshed');
+		const tweetUrlsFromLikes = await getTweetUrlsFromLikes(authorId)
+        const tweetUrlsFromLikesJson = JSON.stringify({...tweetUrlsFromLikes})
+		writeToFile('./twitterLikes.json', tweetUrlsFromLikesJson, returnReply)
+	},
+};
--- utils/twitter.js
+++ utils/twitter.js
@@ -2,6 +2,8 @@ const { TwitterApi } = require('twitter-api-v2')
 const { twitterApiKey } = require('./../config.json')
 const { pipe, andThen, otherwise } = require('ramda')
 
+const twitterClient = new TwitterApi(twitterApiKey);
+
 const tweetUrl = url => new URL(url)
 
 const getTweetId = tweetUrl => {
@@ -9,20 +11,28 @@ const getTweetId = tweetUrl => {
   return urlPaths.includes('status') && urlPaths.slice(-1)[0]
 }
 
-const twitterClient = new TwitterApi(twitterApiKey);
-
 const getTweetObject = async (tweetUrl) => await twitterClient.v2.tweets(getTweetId(tweetUrl), {
-    expansions: ['attachments.media_keys'],
-    'media.fields': ['url'],
-  })
-
+  expansions: ['attachments.media_keys'],
+  'media.fields': ['url'],
+})
 const pullImageFromTweetObj = tweetObject => tweetObject.includes?.media.map(data => data?.url)
-
 const pullTextFromTweetObj = tweetObject => tweetObject?.data[0]?.text
-
 const getTextFromTweet = pipe(tweetUrl, getTweetObject, andThen(pullTextFromTweetObj), otherwise(() => undefined))
-
 const getImagesFromTweet = pipe(tweetUrl, getTweetObject, andThen(pullImageFromTweetObj), otherwise(() => undefined))
+
+const getLikedTweets = async (authorId) => await twitterClient.v2.userLikedTweets(authorId, {
+  expansions: ['author_id']
+});
+const createTweetUrl = (authorId, tweetId) => `https://twitter.com/${authorId}/status/${tweetId}`
+const createLikedTweetsUrls = (likedTweets) => likedTweets.tweets.map(tweet => createTweetUrl(tweet.author_id, tweet.id))
+const getTweetUrlsFromLikes = async (authorId) => {
+  const likedTweets = await getLikedTweets(authorId)
+  for(let i = 0; i < 10; i++) {
+      await likedTweets.fetchNext()
+  }
+  return createLikedTweetsUrls(likedTweets)
+}
 
 exports.getImagesFromTweet = getImagesFromTweet
 exports.getTextFromTweet = getTextFromTweet
+exports.getTweetUrlsFromLikes = getTweetUrlsFromLikes
