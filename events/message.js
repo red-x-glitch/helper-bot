be592b58c652e48c0508242bf89a8599750419c6
swaggy-p-jp <ajattmohamedo@gmail.com>
2022-09-11 22:14:35 -0500

    now using vxtwitter if only one image

--- events/message.js
+++ events/message.js
@@ -6,22 +6,25 @@ const removeExtraRetweet = (msg) => {
     return msg.substring(0, endIndex).trim()
 }
 
-const postImagesToThread = async (retweetUrl, msg) => {
-    const tweetImages = await getImagesFromTweet(retweetUrl)
-    if(tweetImages?.length > 1) {
-        const tweetText = await getTextFromTweet(retweetUrl)
-        const thread = await msg.channel.threads.create({
-            name: tweetText,
-            autoArchiveDuration: 60,
-            reason: 'Images From Tweet',
-        });
-        const webhooks = await msg.channel.fetchWebhooks();
-        const webhook = webhooks.first();
-        tweetImages.forEach(async img => await webhook.send({
-            content: img,
-            threadId: thread.id,
-        }))
-    }
+const postImagesToThread = async (tweetImages, retweetUrl, msg) => {
+    const tweetText = await getTextFromTweet(retweetUrl)
+    const thread = await msg.channel.threads.create({
+        name: tweetText,
+        autoArchiveDuration: 60,
+        reason: 'Images From Tweet',
+    });
+    const webhooks = await msg.channel.fetchWebhooks();
+    const webhook = webhooks.first();
+    tweetImages.forEach(async img => await webhook.send({
+        content: img,
+        threadId: thread.id,
+    }))
+}
+
+const replaceWithVxTwitter = (twitterLink, msg) => {
+    const vxTwitterLink = twitterLink.replace('twitter', 'vxtwitter')
+    msg.delete({ timeout: "1000" })
+    msg.channel.send(vxTwitterLink);
 }
 
 module.exports = {
@@ -29,6 +32,10 @@ module.exports = {
 	async execute(msg) {
         let retweetUrl = msg.content
         if(msg.content.includes('neort')) retweetUrl = removeExtraRetweet(msg.content)
-        if(msg.content.startsWith("https://twitter.com/")) postImagesToThread(retweetUrl, msg)
+        if(msg.content.startsWith("https://twitter.com/")){
+            const tweetImages = await getImagesFromTweet(retweetUrl)
+            if (tweetImages?.length > 1) postImagesToThread(tweetImages, retweetUrl, msg)
+            else replaceWithVxTwitter(retweetUrl, msg)
+        } 
 	},
 };
