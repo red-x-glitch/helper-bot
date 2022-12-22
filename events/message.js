116d10e3878341ceb6b7affe6837b0382d03947a
swaggy-p-jp <ajattmohamedo@gmail.com>
2022-12-22 10:09:01 -0600

    removing thread creation for tweets

--- events/message.js
+++ events/message.js
@@ -1,24 +1,7 @@
-const { getImagesFromTweet, getTextFromTweet } = require('./../utils/twitter.js')
-
 const removeExtraRetweet = (msg) => {
     let endIndex = msg.indexOf('<:neort')
     if (endIndex === -1) endIndex = msg.indexOf(':neort')
     return msg.substring(0, endIndex).trim()
-}
-
-const postImagesToThread = async (tweetImages, retweetUrl, msg) => {
-    const tweetText = await getTextFromTweet(retweetUrl)
-    const thread = await msg.channel.threads.create({
-        name: tweetText.substring(0,99),
-        autoArchiveDuration: 60,
-        reason: 'Images From Tweet',
-    });
-    const webhooks = await msg.channel.fetchWebhooks();
-    const webhook = webhooks.first();
-    tweetImages.forEach(async img => await webhook.send({
-        content: img,
-        threadId: thread.id,
-    }))
 }
 
 const replaceWithFxTwitter = (twitterLink, msg) => {
@@ -32,10 +15,6 @@ module.exports = {
 	async execute(msg) {
         let retweetUrl = msg.content
         if(msg.content.includes('neort')) retweetUrl = removeExtraRetweet(msg.content)
-        if(msg.content.startsWith("https://twitter.com/")){
-            const tweetImages = await getImagesFromTweet(retweetUrl)
-            if (tweetImages?.length > 1) postImagesToThread(tweetImages, retweetUrl, msg)
-            else replaceWithFxTwitter(retweetUrl, msg)
-        } 
+        if(msg.content.startsWith("https://twitter.com/") && tweetImages?.length <= 1) replaceWithFxTwitter(retweetUrl, msg)
 	},
 };
