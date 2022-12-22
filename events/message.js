d624d54dc502c065d742086cad1c6ffc8fd5ad51
swaggy-p-jp <ajattmohamedo@gmail.com>
2022-12-22 13:07:53 -0600

    post to pocket

--- events/message.js
+++ events/message.js
@@ -1,3 +1,6 @@
+const axios = require('axios').default;
+const { consumer_key, access_token } = require('./../config.json');
+
 const removeExtraRetweet = (msg) => {
     let endIndex = msg.indexOf('<:neort')
     if (endIndex === -1) endIndex = msg.indexOf(':neort')
@@ -13,8 +16,19 @@ const replaceWithFxTwitter = (twitterLink, msg) => {
 module.exports = {
 	name: 'messageCreate',
 	async execute(msg) {
-        let retweetUrl = msg.content
-        if(msg.content.includes('neort')) retweetUrl = removeExtraRetweet(msg.content)
-        if(msg.content.startsWith("https://twitter.com/")) replaceWithFxTwitter(retweetUrl, msg)
+        if(msg.channelId == '873250338579308564') {
+            const domain = new URL(msg.content).hostname.replace('www.', '').match(/(.*?)\./)[1]
+            await axios.post('https://getpocket.com/v3/add', {
+                'url': msg.content,
+                'consumer_key': consumer_key,
+                'access_token': access_token,
+                'tags': domain,
+            });
+        }
+        if(msg.content.startsWith("https://twitter.com/")) {
+            let retweetUrl = msg.content
+            if(msg.content.includes('neort')) retweetUrl = removeExtraRetweet(msg.content)
+            replaceWithFxTwitter(retweetUrl, msg)
+        } 
 	}
 };
