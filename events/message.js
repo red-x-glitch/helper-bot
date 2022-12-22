3337f8d0878a5da49cfb39411625d8494bcb7140
swaggy-p-jp <ajattmohamedo@gmail.com>
2022-12-22 10:13:01 -0600

    refactor and fix

--- events/message.js
+++ events/message.js
@@ -1,8 +1,4 @@
-const removeExtraRetweet = (msg) => {
-    let endIndex = msg.indexOf('<:neort')
-    if (endIndex === -1) endIndex = msg.indexOf(':neort')
-    return msg.substring(0, endIndex).trim()
-}
+const { getImagesFromTweet } = require('./../utils/twitter.js')
 
 const replaceWithFxTwitter = (twitterLink, msg) => {
     const vxTwitterLink = twitterLink.replace('twitter', 'fxtwitter')
@@ -13,8 +9,7 @@ const replaceWithFxTwitter = (twitterLink, msg) => {
 module.exports = {
 	name: 'messageCreate',
 	async execute(msg) {
-        let retweetUrl = msg.content
-        if(msg.content.includes('neort')) retweetUrl = removeExtraRetweet(msg.content)
-        if(msg.content.startsWith("https://twitter.com/") && tweetImages?.length <= 1) replaceWithFxTwitter(retweetUrl, msg)
+        const tweetImages = await getImagesFromTweet(msg.content)
+        if(msg.content.startsWith("https://twitter.com/") && tweetImages?.length <= 1) replaceWithFxTwitter(msg.content, msg)    
 	},
 };
