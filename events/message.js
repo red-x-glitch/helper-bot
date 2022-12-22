fcf9f5e2285c50e93f1bd6b6e0203bd7b44174d2
swaggy-p-jp <ajattmohamedo@gmail.com>
2022-12-22 12:06:54 -0600

    removing twitter helpers. using fxtwitter

--- events/message.js
+++ events/message.js
@@ -1,15 +1,12 @@
-const { getImagesFromTweet } = require('./../utils/twitter.js')
-
-const replaceWithFxTwitter = (twitterLink, msg) => {
-    const vxTwitterLink = twitterLink.replace('twitter', 'fxtwitter')
-    msg.delete({ timeout: "1000" })
-    msg.channel.send(vxTwitterLink);
-}
+const { replaceWithFxTwitter } = require('../utils/messageHelpers.js')
 
 module.exports = {
 	name: 'messageCreate',
 	async execute(msg) {
-        const tweetImages = await getImagesFromTweet(msg.content)
-        if(msg.content.startsWith("https://twitter.com/") && tweetImages?.length <= 1) replaceWithFxTwitter(msg.content, msg)    
-	},
+        if(msg.content.startsWith("https://twitter.com/")) {
+            const vxTwitterLink = twitterLink.replace('twitter', 'fxtwitter')
+            msg.delete({ timeout: "1000" })
+            msg.channel.send(vxTwitterLink);
+        }
+	}
 };
--- utils/twitter.js
+++ utils/twitter.js
@@ -1,24 +1,7 @@
 const { TwitterApi } = require('twitter-api-v2')
 const { twitterApiKey } = require('./../config.json')
-const { pipe, andThen, otherwise } = require('ramda')
 
 const twitterClient = new TwitterApi(twitterApiKey);
-
-const tweetUrl = url => new URL(url)
-
-const getTweetId = tweetUrl => {
-  const urlPaths = tweetUrl.pathname.split("/")
-  return urlPaths.includes('status') && urlPaths.slice(-1)[0]
-}
-
-const getTweetObject = async (tweetUrl) => await twitterClient.v2.tweets(getTweetId(tweetUrl), {
-  expansions: ['attachments.media_keys'],
-  'media.fields': ['url'],
-})
-const pullImageFromTweetObj = tweetObject => tweetObject.includes?.media.map(data => data?.url)
-const pullTextFromTweetObj = tweetObject => tweetObject?.data[0]?.text
-const getTextFromTweet = pipe(tweetUrl, getTweetObject, andThen(pullTextFromTweetObj), otherwise(() => undefined))
-const getImagesFromTweet = pipe(tweetUrl, getTweetObject, andThen(pullImageFromTweetObj), otherwise(() => undefined))
 
 const getLikedTweets = async (authorId) => await twitterClient.v2.userLikedTweets(authorId, {
   expansions: ['author_id']
@@ -33,6 +16,4 @@ const getTweetUrlsFromLikes = async (authorId) => {
   return createLikedTweetsUrls(likedTweets)
 }
 
-exports.getImagesFromTweet = getImagesFromTweet
-exports.getTextFromTweet = getTextFromTweet
 exports.getTweetUrlsFromLikes = getTweetUrlsFromLikes
