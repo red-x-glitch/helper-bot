5d6be0782b3351ef69a243de6b47553e42d2b832
swaggy-p-jp <ajattmohamedo@gmail.com>
2023-02-14 07:03:47 -0600

    wrapping in an express app

--- index.js
+++ index.js
@@ -2,32 +2,46 @@
 const { Client, Collection, Intents } = require('discord.js');
 const fs = require('fs');
 const { token } = require('./config');
+const express = require('express')
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-client.commands = new Collection();
+const app = express()
+const port = 3000
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+app.get('/', (req, res) => {
+  // Create a new client instance
+	const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
-}
+	client.commands = new Collection();
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+	const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
+	for (const file of commandFiles) {
+		const command = require(`./commands/${file}`);
+		// Set a new item in the Collection
+		// With the key as the command name and the value as the exported module
+		client.commands.set(command.data.name, command);
 	}
-	else {
-		client.on(event.name, (...args) => event.execute(...args, client.commands));
-	}
-}
 
-// Login to Discord with your client's token
-client.login(token);
+	const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+	for (const file of eventFiles) {
+		const event = require(`./events/${file}`);
+		if (event.once) {
+			client.once(event.name, (...args) => event.execute(...args));
+		}
+		else {
+			client.on(event.name, (...args) => event.execute(...args, client.commands));
+		}
+	}
+
+	// Login to Discord with your client's token
+	client.login(token);
+
+	res.send('Ready! Logged in as Helper#0396')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
